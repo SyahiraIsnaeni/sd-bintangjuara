8eb56d0c75ad78ae5d5143e006862bebb54af9b3
Debora Oktaria Lubis <deboraoktarialubis@gmail.com>
2023-02-14 18:54:53 +0700

    guru

---
+++ resources/views/frontend/guru.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script src="https://kit.fontawesome.com/a59b9b09ab.js" crossorigin="anonymous"></script>
+      <title>BINTANG JUARA</title>
+      <!-- add icon link -->
+      <link rel="icon" href="assets/logo1.png" type="image/x-icon" />
+      <link rel="icon" href="2.jpeg" />
+      <!-- Swiper CSS -->
+      <link rel="stylesheet" type="text/css" href="stylegallery.css" />
+      <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" crossorigin="anonymous" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+    </head>
+    <style></style>
+  </head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color: #45b0f8">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">
+          <img src="assets/logo1.png" alt="logo" height="50" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.html">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://sd.bintangjuara.sch.id/772-2/">Yuk Wakaf</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://ppdb.bintangjuara.sch.id/">PPDB</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kontak.html">Kontak</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- team guru -->
+    <section id="Fakta">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="row g-3">
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br />
+            <div class="row g-3">
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12 col-md-6 col-lg-3">
+                <img src="assets/dummy.jpeg" class="card-img-top" alt="news" style="margin-bottom: 5%" />
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                      Debora Oktaria<br />
+                      21120120120032
+                    </h5>
+                    <p class="card-text">OWNER</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- akhir team guru -->
+  </body>
+</html>
